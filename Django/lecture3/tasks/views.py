081772d3e225345b6c22ef5531203101a26a8a04
MariusJM <mariusjm.code@gmail.com>
2024-01-18 23:49:45 +0200

    add tasks to the list

--- Django/lecture3/tasks/views.py
+++ Django/lecture3/tasks/views.py
@@ -1,11 +1,31 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
 
-tasks = ["foo", "bar", "baz"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New task")
+    #priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
+#tasks = ["foo", "bar", "baz"]
+tasks = []
 # Create your views here.
-def index(requests):
-    return render(requests, "tasks/index.html",{
+def index(request):
+    return render(request, "tasks/index.html",{
         "tasks": tasks
     })
 
-def add(requests):
-    return render(requests, "tasks/add.html")
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html",{
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
