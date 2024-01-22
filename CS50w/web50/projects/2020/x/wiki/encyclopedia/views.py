6fd2343fe6643bed6d84e067edb1a95d647fa1eb
MariusJM <mariusjm.code@gmail.com>
2024-01-22 14:25:23 +0200

    add md to html convertsion function

--- CS50w/web50/projects/2020/x/wiki/encyclopedia/views.py
+++ CS50w/web50/projects/2020/x/wiki/encyclopedia/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-
 from . import util
-
+from markdown import markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def markdown_to_html(title):
+    title_saved = util.get_entry(title)
+    markdowner = markdown()
+
+    if title_saved == None:
+        return None
+    else:
+        return markdowner.convert(title_saved)
