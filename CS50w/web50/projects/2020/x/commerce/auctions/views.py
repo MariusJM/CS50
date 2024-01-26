d6b6da845a0f546ddccf43987c1a53d45cd38ca1
MariusJM <mariusjm.code@gmail.com>
2024-01-26 16:42:47 +0200

    add create listing functionality

--- CS50w/web50/projects/2020/x/commerce/auctions/views.py
+++ CS50w/web50/projects/2020/x/commerce/auctions/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User, Category
+from .models import User, Category, Listing
 
 
 def index(request):
@@ -63,18 +63,32 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def create_listing(request):
-    if request.method== "POST":
+    if request.method=="GET":
+        return render(request, "auctions/create_listing.html",{
+            "categories": Category.objects.values_list('category_name', flat=True)
+        })
+    else:
+        # request.method== "POST":
         user = request.user
         category = request.POST["category"]
+        category_d = Category.objects.get(category_name=category)
         title = request.POST["title"]
         image = request.POST["image"]
         starting_bid = request.POST["starting_bid"]
         description = request.POST["description"]
         is_active = request.POST["is_active"]
-
-    return render(request, "auctions/create_listing.html",{
-        "categories": Category.objects.all()
-    })
+        add_listing = Listing(
+            seller = user,
+            category = category_d,
+            title = title,
+            image = image,
+            starting_bid = int(starting_bid),
+            description = description,
+            isActive = bool(is_active)
+        )
+        add_listing.save()
+        return HttpResponseRedirect(reverse(index))
+    
 
 def watchlist(request):
     return render(request, "auctions/watchlist.html")
