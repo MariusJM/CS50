f44fcfe7dc6ddc6d8ba4f3306569cf1e3524e3aa
MariusJM <mariusjm.code@gmail.com>
2024-02-08 13:49:44 +0200

    add archiving logic

--- CS50w/web50/projects/2020/x/mail/mail/static/mail/inbox.js
+++ CS50w/web50/projects/2020/x/mail/mail/static/mail/inbox.js
@@ -6,15 +6,18 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector("#send").addEventListener("click", send_mail);
-
+  
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
 
 function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
+  document.querySelector('#mail-buttons').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -29,6 +32,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email-view').style.display = 'none';
+  document.querySelector('#mail-buttons').style.display = 'none';
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
@@ -59,6 +63,8 @@ function view_mail(id){
   console.log("Trying to view email item")
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#email-view').style.display = 'block';
+  const user = document.querySelector("h2").innerHTML
+  document.querySelector('#mail-buttons').style.display = 'block';
   
   fetch(`/emails/${id}`)
     .then(response => response.json())
@@ -71,15 +77,62 @@ function view_mail(id){
       document.querySelector(".subject").innerHTML = email.subject
       document.querySelector(".timestamp").innerHTML = email.timestamp
       document.querySelector(".body").innerHTML = email.body
-    });
+
+	  const sender = email.sender;
+		if (user === sender){
+      document.querySelector('#mail-buttons').style.display = 'none';
+    } else if (email.archived){
+      document.querySelector(".archive-button").innerHTML = "Unarchive"
+      document.querySelector(".archive-button").id = "unarchive"
+      document.querySelector(".archive-button").addEventListener("click", () => unarchive_mail(id))
+    } else {
+      document.querySelector(".archive-button").innerHTML = "Archive"
+      document.querySelector(".archive-button").id = "archive"
+      document.querySelector(".archive-button").addEventListener("click", () => archive_mail(id))
+    }
+
+
+  });
+    
   fetch(`/emails/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
         read: true
     })
   })
+  
 }
 
+function archive_mail(id){
+  console.log(`Trying to archive mail ${id}`)
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: true
+    })
+  })
+  .then(result => {
+    // Print result
+    console.log(result);
+    load_mailbox('inbox')
+  })
+}
+
+function unarchive_mail(id){
+  console.log(`Trying to unarchive mail ${id}`)
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: false
+    })
+  })
+  .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('inbox')
+  })
+  
+}
 
 function send_mail(event){
   event.preventDefault();
