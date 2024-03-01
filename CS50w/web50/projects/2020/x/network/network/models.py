c074b5d2b2a32235d519fcaf13e66753ef6a6755
MariusJM <mariusjm.code@gmail.com>
2024-03-01 21:34:58 +0200

    add tweet model

--- CS50w/web50/projects/2020/x/network/network/models.py
+++ CS50w/web50/projects/2020/x/network/network/models.py
@@ -3,4 +3,25 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="following")
+
+
+class tweet(models.Model):
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="author")
+    body = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, blank=True, related_name="likes")
+
+    def __str__ (self):
+        return f"{self.author} - {self.date_created}"
+    
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "author" : self.author.username,
+            "body" : self.body,
+            "date_created" : self.date_created.strftime("%Y-%m-%d %H:%M:%S"),
+            "date_edited" : self.date_edited.strftime("%Y-%m-%d %H:%M:%S"),
+            "likes" : self.likes.count(),
+        }
